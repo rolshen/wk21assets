5ecc9dfae890b38ab128ade8a11a6e099e303692
Randy Olshen <rolshen@aol.com>
2020-04-18 12:19:12 -0700

    update

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+const Footer = () => (
+	<footer className = "page-footer">
+		<div className = "container-fluid text-center">
+			<p>Randy Olshen &copy; 2020</p>
+		</div>	
+	</footer>
+);
+
+export default Footer;
